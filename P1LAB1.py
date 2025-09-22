21582b7bd54362516c8c68a7d22a3bd7ddfe7c3d
munizj9259 <munizj9259@student.faytechcc.edu>
2025-09-21 20:34:03 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+# Name: [Jovanny Muniz]
+# Date: [09/21/25]
+# Assignment Name: P1LAB1
+# Description: Program asks user for their first and last name separately,
+#              then prints a welcome message.
+
+# Prompt user for first and last name separately
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Hello,", Jovanny, Muniz + "! Welcome to CTI-110")
